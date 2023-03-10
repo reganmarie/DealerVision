a472116e70ec18f6ec999f03a561f617180aaea8
Jackie <jwliu24@gmail.com>
2023-03-10 09:47:03 -0800

    updated technician ad manufacturer list

--- ghi/app/src/ServicesList.js
+++ ghi/app/src/ServicesList.js
@@ -47,43 +47,45 @@ function ServicesList(props) {
     }
 
     return (
-        <div>
-        <h1>Service appointments</h1>
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>VIN</th>
-                    <th>Owner Name</th>
-                    <th>Date Time</th>
-                    <th>Assigned Technician</th>
-                    <th>Reason</th>
-                    <th>Complete</th>
-                    <th>Cancel</th>
-                    <th>VIP</th>
-                </tr>
-            </thead>
-            <tbody>
-                {services.map(service => {
-                    return (
-                        <tr key={service.href}>
-                            <td>{ service.auto.vin }</td>
-                            <td>{ service.owner_name}</td>
-                            <td>{ service.date }</td>
-                            <td>{ service.technician.name }</td>
-                            <td>{ service.reason }</td>
-                            <td>
-                                <button type="button" className="btn btn-success" onClick={() => finishService(service)}>Finish</button>
-                            </td>
-                            <td>
-                                <button type="button" className="btn btn-danger" onClick={() => cancelService(service)}>Cancel</button>
-                            </td>
-                            <td>{ service.isVIP ? "yes" : "no" }</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-        </div>
+        <>
+            <div>
+            <h1>Service appointments</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>VIN</th>
+                        <th>Owner Name</th>
+                        <th>Date Time</th>
+                        <th>Assigned Technician</th>
+                        <th>Reason</th>
+                        <th>Complete</th>
+                        <th>Cancel</th>
+                        <th>VIP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {services.map(service => {
+                        return (
+                            <tr key={service.href}>
+                                <td>{ service.auto.vin }</td>
+                                <td>{ service.owner_name}</td>
+                                <td>{ service.date }</td>
+                                <td>{ service.technician.name }</td>
+                                <td>{ service.reason }</td>
+                                <td>
+                                    <button type="button" className="btn btn-success" onClick={() => finishService(service)}>Finish</button>
+                                </td>
+                                <td>
+                                    <button type="button" className="btn btn-danger" onClick={() => cancelService(service)}>Cancel</button>
+                                </td>
+                                <td>{ service.isVIP ? "yes" : "no" }</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            </div>
+        </>
     )
 }
 
--- ghi/app/src/TechniciansList.js
+++ ghi/app/src/TechniciansList.js
@@ -1,28 +1,49 @@
-function TechniciansList (props) {
-    return (
-      <div>
-        <h1>Technicians</h1>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Employee ID</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.technicians.map(technician => {
-              return (
-                <tr key={technician.id}>
-                  <td>{ technician.name }</td>
-                  <td>{ technician.id }</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        </div>
-      );
+import { useEffect, useState } from "react";
 
+function TechniciansList (props) {
+  const [technicians, setTechnicians] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'http://localhost:8080/api/technicians/';
+
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        const technicians = data.technicians;
+        setTechnicians(technicians);
+      } else {
+        console.error('error');
+      }
+    };
+    fetchData();
+  }, [])
+
+    return (
+      <>
+        <div>
+          <h1>Technicians</h1>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Employee ID</th>
+              </tr>
+            </thead>
+            <tbody>
+              {technicians.map(technician => {
+                return (
+                  <tr key={technician.id}>
+                    <td>{ technician.name }</td>
+                    <td>{ technician.id }</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
 }
 
 export default TechniciansList;
