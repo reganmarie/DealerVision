2b422102a4be296e0ae41aac65bb97bbd7f4f88c
Regan Tewksbury <regantewksbury@gmail.com>
2023-05-11 17:08:30 -0700

    Added drop down to organize navbar and image carousel to home page

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,12 +1,33 @@
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">CarCar</h1>
+      <h1 className="display-5 fw-bold">DealerVision</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
           The premiere solution for automobile dealership
           management!
         </p>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://images.pexels.com/photos/754595/pexels-photo-754595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="Defender in a field"/>
+            </div>
+            <div class="carousel-item">
+              <img src="https://images.pexels.com/photos/2356071/pexels-photo-2356071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="Car on a mountainside"/>
+            </div>
+            <div class="carousel-item">
+              <img src="https://images.pexels.com/photos/2127039/pexels-photo-2127039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="BMW in a showroom"/>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </div>
   );
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -2,9 +2,9 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
     <div className="container-fluid">
-      <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+      <NavLink className="navbar-brand" to="/">DealerVision</NavLink>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -13,52 +13,72 @@ function Nav() {
             <li className="nav-item">
               <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="new-manufacturer" to="/manufacturers/new">New Manufacturer</NavLink>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Inventory
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="new-manufacturer" to="/manufacturers/new">New Manufacturer</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="manufacturer" to="/manufacturers">Manufacturers</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="new-model" to="/models/new">New Model</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="models" to="/models">Models</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="new-automobile" to="/automobiles/new">New Automobile</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="automobiles" to="/automobiles">Automobiles</NavLink>
+                </li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="manufacturer" to="/manufacturers">Manufacturers</NavLink>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Servicing
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="new-technician" to="/technicians/new">New technician</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" to="/technicians">Technicians</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="new-service" to="/services/new">New service</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" to="/services">Services</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="history" to="/services/history">Service History</NavLink>
+                </li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="new-model" to="/models/new">New Model</NavLink>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                 Sales
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="new-customer" to="/customer/new">New Customer</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="new-salesperson" to="/salesperson/new">New Salesperson</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="new-salesrecord" to="/salesrecords/new">New Sales Record</NavLink>
+                </li>
+                <li className="dropdown-item">
+                  <NavLink className="nav-link" aria-current="page" id="salesrecords" to="/salesrecords">Sales Record History, Searchable</NavLink>
+                </li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="models" to="/models">Models</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="new-automobile" to="/automobiles/new">New Automobile</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="automobiles" to="/automobiles">Automobiles</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="new-technician" to="/technicians/new">New technician</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/technicians">Technicians</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="new-service" to="/services/new">New service</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/services">Services</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="history" to="/services/history">Service History</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="new-customer" to="/customer/new">New Customer</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="new-salesperson" to="/salesperson/new">New Salesperson</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="new-salesrecord" to="/salesrecords/new">New Sales Record</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" id="salesrecords" to="/salesrecords">Sales Record History, Searchable</NavLink>
-            </li>
-
           </ul>
         </div>
       </div>
